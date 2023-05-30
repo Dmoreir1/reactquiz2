7b9f1a60e10b168317abe5306489c51cbd1375f4
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-30 10:43:25 -0400

    number builder

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useState } from 'react'
+
+// React component that builds string of numbers that represent 
+// how many times add numbers button pressed; reset clears
+
 
 export const NumberBuilder = () => {
+    let [count, setCount] = useState (1);
+    let [numbers, setNumbers] = useState ('');
+    const addNumber = () => {
+        setCount(count+1);
+        setNumbers(numbers + count);
+    };
+
+    const resetCount = () => {
+        setCount(1); 
+        setNumbers('');
+    };
 
     return (
         <>
-            <button>Add Number</button>
-            <button>Reset</button>
+            <button onClick = {addNumber}>Add Number</button>
+            <button onClick = {resetCount}>Reset</button>
             <br></br>
-            Numbers: 
+            Numbers: {numbers}
         </>
     )
 }
